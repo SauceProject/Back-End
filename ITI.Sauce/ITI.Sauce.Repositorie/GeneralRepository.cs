024cd42b5ddd133130e9208b0f2f6fc2f42e1f01
a1b1f <ahmedrafie3@gmail.com>
2022-06-20 19:29:00 +0200

    Last Uplode

---
+++ ITI.Sauce/ITI.Sauce.Repositorie/GeneralRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using ITI.Sauce.Models;
+
+namespace ITI.Library.Repositories
+{
+    public class GeneralRepository<T> where T : class
+    {
+        private readonly DBContext dbContext;
+        private DbSet<T> Set;
+        public GeneralRepository()
+        {
+            dbContext = new DBContext();
+            Set = dbContext.Set<T>();
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> filter = null, string orderBy = null, bool isAscending = false
+                    , int pageIndex = 1, int pageSize = 20
+            , params string[] includProps)
+        {
+            var query = Set.AsQueryable();
+            foreach (string prop in includProps)
+                query = query.Include(prop);
+            if (filter != null)
+                query = query.Where(filter);
+            if (orderBy != null)
+                query = query.OrderBy(orderBy, isAscending);
+            int rowsCount = query.Count();
+            if (rowsCount < pageSize)
+                pageIndex = 1;
+            if (pageIndex <= 0)
+                pageIndex = 1;
+            int excludedRowsCount = (pageIndex - 1) * pageSize;
+            query = query.Skip(excludedRowsCount).Take(pageSize);
+            return query;
+        }
+
+        public IQueryable<T> GetList()
+        {
+            return Set.AsQueryable();
+        }
+        public IQueryable<T> GetByID(int ID)
+        {
+            return Set.AsQueryable().Where(e => e.Equals(ID));
+        }
+    }
+}
---
+++ ITI.Sauce/ITI.Sauce.ViewModels/Shared/PaginingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Library.ViewModels.Shared
+{
+    public class PaginingViewModel<T>
+    {
+        public int Count { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public T Data { get; set; }
+    }
+}
+    
