49330ab79cbe2f6c80ec28cdb7349e470ff3c3e1
BishoyGaidSobhy <bishoy.gaid2020@gmail.com>
2022-06-22 13:19:06 +0200

    hi

---
+++ ITI.Sauce/ITI.Sauce.Repositorie/OrderRepository.cs
@@ -0,0 +1,65 @@
+﻿
+using ITI.Sauce.Models;
+using ITI.Sauce.Repositories;
+using ITI.Sauce.ViewModels;
+using ITI.Sauce.ViewModels.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abp.Linq.Expressions;
+
+namespace ITI.Sauce.Repositorie
+{
+    public class OrderRepository : GeneralRepository<Order>
+    {
+        public PaginingViewModel<List<OrderViewModel>> Get(int ID = 0, string orderBy = null
+            , bool isAscending = false, string NameEN = "",
+            string NameAR = "", DateTime? registerDate =null 
+            ,int pageIndex = 1, int pageSize = 20)
+                    
+        {
+            
+            var filter = PredicateBuilder.New<Order>();
+            var oldFiler = filter;
+            if (ID > 0)
+                filter = filter.Or(o => o.ID == ID);
+            if (!string.IsNullOrEmpty(NameEN))
+                filter = filter.Or(o => o.NameEN.Contains(NameEN));
+            if (!string.IsNullOrEmpty(NameAR))
+                filter = filter.Or(o => o.NameAR.Contains(NameAR));
+            if (registerDate != null) 
+                filter = filter.Or(o => o.registerDate.Year == registerDate.Value.Year);
+            
+            if (filter == oldFiler)
+                filter = null;
+            var query = base.Get(filter, orderBy, isAscending, pageIndex, pageSize);
+
+
+            var result =
+            query.Select(i => new OrderViewModel
+            {
+                ID = i.ID,
+                NameEN = i.NameEN,
+                NameAR = i.NameAR,
+                registerDate = i.registerDate.ToLongTimeString(),
+
+            });
+
+            PaginingViewModel<List<OrderViewModel>>
+               finalResult = new PaginingViewModel<List<OrderViewModel>>()
+               {
+                   PageIndex = pageIndex,
+                   PageSize = pageSize,
+                   Count = base.GetList().Count(),
+                   Data = result.ToList()
+               };
+
+
+            return finalResult;
+        }
+        
+    }
+}
---
+++ ITI.Sauce/ITI.Sauce.ViewModels/Order/OrderViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Sauce.ViewModels
+{
+    public class OrderViewModel
+    {
+
+        public int ID { get; set; }
+      
+        public string NameEN { get; set; }
+        public string NameAR { get; set; }
+        public string registerDate { get; set; }
+        public int IsDeleted { get; set; }
+    }
+}
