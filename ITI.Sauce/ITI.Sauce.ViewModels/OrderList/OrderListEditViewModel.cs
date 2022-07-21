8fc617461789506f6fc2475b16687739295f842c
a1b1f <ahmedrafie3@gmail.com>
2022-07-21 18:14:18 +0200

    OrderList is done

--- ITI.Sauce/ITI.Sauce.ViewModels/OrderList/OrderListEditViewModel.cs
+++ ITI.Sauce/ITI.Sauce.ViewModels/OrderList/OrderListEditViewModel.cs
@@ -4,51 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ITI.Sauce.Models;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace ITI.Sauce.ViewModels
 {
     public static partial class OrderListExtensions
     {
-        public static OrderListViewModel ToViewModel(this OrderList model)
+        public static OrderList ToModel(this OrderListEditViewModel model)
         {
-            return new OrderListViewModel
+            return new OrderList
             {
                 OrderListID = model.OrderListID,
                 OrderListQty = model.OrderListQty,
                 OrderListPrice = model.OrderListPrice,
-                RecipeID = model.Recipe_ID,
+                Recipe_ID = model.RecipeID,
                 OrderID = model.OrderID,
             };
+
         }
-
-        public static OrderListEditViewModel ToEditViewModel(this OrderListViewModel model)
-        {
-
-
-            return new OrderListEditViewModel()
-            {
-
-                OrderListID = model.OrderListID,
-                OrderListQty = model.OrderListQty,
-                OrderListPrice = model.OrderListPrice,
-                RecipeID = model.RecipeID,
-                OrderID = model.OrderID,
-
-            };
-        }
-
-
     }
-
-
-
-    public class OrderListViewModel
+    public class OrderListEditViewModel
     {
         public int OrderListID { get; set; }
         public int OrderListQty { get; set; }
         public float OrderListPrice { get; set; }
         public int RecipeID { get; set; }
         public int OrderID { get; set; }
-        
+
     }
 }
---
+++ ITI.Sauce/ITI.sauce.cs/API/OrderListAPIController.cs
@@ -0,0 +1,32 @@
+﻿using ITI.Sauce.Repository;
+using ITI.Sauce.ViewModels;
+using ITI.Sauce.ViewModels.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ITI.Sauce.MVC.API
+{
+    public class OrderListAPIController : ControllerBase
+    {
+        private readonly OrderListRepository orderListRepository;
+        public OrderListAPIController(OrderListRepository _orderListRepository)
+        {
+            orderListRepository = _orderListRepository;
+        }
+        [HttpGet]
+        public ResultViewModel Get()
+        {
+            var OrderListInfo = orderListRepository.Get();
+            return new ResultViewModel { Data = OrderListInfo, Success = true };
+
+        }
+
+        [HttpPost]
+        public ResultViewModel Add([FromBody] OrderListEditViewModel model)
+        {
+            var result = orderListRepository.Add(model);
+            return new ResultViewModel { Data = result, Success = true };
+        }
+    }
+}
