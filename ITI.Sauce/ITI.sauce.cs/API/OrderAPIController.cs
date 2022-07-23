1b3f90b58052a466e9b897c53d31b9659c370d4c
BasmaEsmail <basmaabas755@gmail.com>
2022-07-23 22:27:18 +0200

    cart and order

--- ITI.Sauce/ITI.sauce.cs/API/OrderAPIController.cs
+++ ITI.Sauce/ITI.sauce.cs/API/OrderAPIController.cs
@@ -17,12 +17,20 @@ namespace ITI.sauce.MVC.Controllers
     {
         OrderRepository ordRepo;
         private readonly UnitOfWork UnitOfWork;
+        CartRepository cartRepository;
+        RecipeRepository recipeRepository;
+        OrderListRepository orderListRepository;
 
-        public OrderAPIController(OrderRepository _ordRepo, UnitOfWork _unitOfWork)
+        public OrderAPIController(OrderRepository _ordRepo, CartRepository _cartRepository,
+             RecipeRepository _recipeRepository, OrderListRepository _orderListRepository, UnitOfWork _unitOfWork)
         {
             //DBContext dBContext = new DBContext();
             ordRepo = _ordRepo;
             UnitOfWork = _unitOfWork;
+            cartRepository = _cartRepository;
+            recipeRepository= _recipeRepository;
+            orderListRepository = _orderListRepository;
+
         }
         public ResultViewModel Get(int ID = 0, string orderBy = null, bool isAscending = false,
             string UserId = "", DateTime? registerDate = null, int pageIndex = 1, int pageSize = 20)
@@ -68,8 +76,18 @@ namespace ITI.sauce.MVC.Controllers
         [HttpPost]
         public ResultViewModel Add([FromBody] OrderEditViewModel model)
         {
-            if (ModelState.IsValid == true)
+            var userCarts = cartRepository.GetByUser(model.UserId);
+            int i = 0;
+            foreach(var c in userCarts.Data)
             {
+                var recipe = recipeRepository.GetOne(c.Recipe_ID);
+
+                model.orderLists[i].OrderListPrice = recipe.Price;
+                model.orderLists[i].OrderListQty = c.Qty;
+
+                i++;
+            }
+
                 ordRepo.Add(model);
                 UnitOfWork.Save();
                 return new ResultViewModel()
@@ -78,21 +96,9 @@ namespace ITI.sauce.MVC.Controllers
                     Success = true,
                     Data = null
                 };
-            }
-            else
-                return new ResultViewModel()
-                {
-                    Message = "Added Succesfully",
-                    Success = true,
-                    Data = null
-                };
+           
         }
 
-
-     
-
-
-      
 
         [HttpGet]
         public ResultViewModel Remove(OrderEditViewModel model, int ID)
